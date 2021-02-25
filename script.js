4d673e597911e80178b23afaad7c2340148de25d
Leonardo <leonardofabbri125@gmail.com>
2021-02-25 18:22:18 +0100

    definitivo

---
+++ script.js
@@ -0,0 +1,15 @@
+
+for (var i=1; i<=100;i++) {
+    //2
+    if(i%3==0) {
+    //3
+    console.log ("fizz");
+    }
+    //4
+    else if(i%5==0) {
+       console.log ("BUZZ");
+    }
+    else {
+        console.log (i);
+    }
+} 
